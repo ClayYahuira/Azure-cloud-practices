df03e84637842c45c9682975cc3e51ebc22325d9
Clay Yahuira <clay.achahui@gmail.com>
2026-09-21 22:06:11 +0200

    Add https only for the creation of app service

--- Az-app-service/Infra/provision.sh
+++ Az-app-service/Infra/provision.sh
@@ -9,4 +9,4 @@ echo ">> creating App Service Plan $SKU"
 az appservice plan create --name "$PLAN" --resource-group "$RG" --sku "$SKU" --is-linux --location "$LOCATION" --output table
 
 echo ">> creating App Service $APP"
-az webapp create --name "$APP" --resource-group "$RG" --plan "$PLAN" --runtime "DOTNETCORE:9.0" --output table
+az webapp create --name "$APP" --resource-group "$RG" --plan "$PLAN" --runtime "DOTNETCORE:9.0" --https-only true --output table
--- Az-app-service/Infra/teardown.sh
+++ Az-app-service/Infra/teardown.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 source "$(dirname "$0")/variables.sh"
 
 echo ">> Deleting Resource group $RG"
-az group delete --name "$RG" --yes --no-wait
+az group delete --name "$RG" --yes 
